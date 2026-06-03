b060d87de5775aa4e9f32bc93d5372433d6d64f3
Nguyễn Duy Tiến <151917014+duytien05@users.noreply.github.com>
2026-06-04 01:44:48 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,5 @@
-import org.gradle.api.tasks.Delete
-import org.gradle.api.file.Directory
-
 plugins {
-    id("com.google.gms.google-services") version "4.4.4" apply false
+    id "com.google.gms.google-services" version "4.4.2" apply false
 }
 
 allprojects {
@@ -12,22 +9,15 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-
-rootProject.layout.buildDirectory.value(newBuildDir)
-
+rootProject.buildDir = "../build"
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.buildDir = "${rootProject.buildDir}/${project.name}"
 }
-
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+tasks.register("clean", Delete) {
+    delete rootProject.buildDir
 }
+
